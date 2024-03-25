93ce0190feae7ef6b77dfafc94f2f1edffabd26a
Lucas <132510177+lucasvna@users.noreply.github.com>
2024-03-25 12:09:05 -0300

    add q2

---
+++ Notas de aula/Nota de aula 1/Questão 2/Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno { 
+    
+    String nome; 
+
+    Double nota_1;
+    Double nota_2;
+    Double nota_3;
+    Double media;
+
+    public void calcular_media(){ 
+
+        media = (nota_1+nota_2+nota_3) / 3;
+
+    }
+
+}
