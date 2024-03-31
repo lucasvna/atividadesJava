90c2b03651673a50b1ebfd3a23616616b93e23a7
Lucas <132510177+lucasvna@users.noreply.github.com>
2024-03-31 15:32:47 -0300

    Create Funcionarios.java

---
+++ Notas de aula/Nota de aula 3/Questão 1/Funcionarios.java
@@ -0,0 +1,22 @@
+public class Funcionarios { 
+    
+        String nome;
+        double salario;
+        
+        public String getNome() {
+            return this.nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public double getSalario() {
+            return this.salario;
+        }
+
+        public void setSalario(double salario) {
+            this.salario = salario;
+        } 
+
+}
