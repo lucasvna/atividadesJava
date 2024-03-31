d33a112f62cd721685ce3750d1a031428af28f38
Lucas <132510177+lucasvna@users.noreply.github.com>
2024-03-31 18:26:22 -0300

    Create Carro.java

---
+++ Notas de aula/Nota de aula 4/Questão 3/Carro.java
@@ -0,0 +1,36 @@
+public class Carro {
+
+    protected String marca, modelo;
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Carro(String marca, String modelo) {
+
+        this.marca = marca;
+        this.modelo = modelo;
+
+    }
+
+    public void mostrarDetalhes() {
+
+        System.out.println("Informações sobre o carro: ");
+        System.out.println();
+        System.out.println("Marca: " + getMarca() + " Modelo: " + getModelo());
+
+    }
+
+}
