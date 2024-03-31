6e0c2a14eacc75b7f71432d7ef5ecf02432cd374
Lucas <132510177+lucasvna@users.noreply.github.com>
2024-03-31 18:21:22 -0300

    Create Produto.java

---
+++ Notas de aula/Nota de aula 4/Questão 2/Produto.java
@@ -0,0 +1,14 @@
+public class Produto {
+
+    protected String peca, modelo;
+    protected double preco;
+
+    public Produto(String peca, double preco, String modelo) {
+
+        this.peca = peca;
+        this.preco = preco;
+        this.modelo = modelo;
+
+    }
+
+}
